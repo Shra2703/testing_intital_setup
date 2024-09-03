b70fe44c225d5a34f5c58bf2a8ce8b67770e385f
Shraddha Goyal <shraddhagoyal800@gmail.com>
2024-09-03 12:16:39 +0530

    some changes

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import PitchDetector from "./PitchDetector";
+
+function App() {
+  return (
+    <>
+      <div>
+        <PitchDetector />
+      </div>
+    </>
+  );
+}
+
+export default App;
